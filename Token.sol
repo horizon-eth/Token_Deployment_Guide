da05946edcd68d3f33a58762a251b526fd033165
Horizon <ufuk.eth@gmail.com>
2025-05-09 14:09:38 +0300

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract YOUR_TOKEN_CONTRACT_NAME is ERC20 {
-    constructor(string memory _name, string memory _symbol, uint256 _amount, address _receiver) ERC20(_name, _symbol) {
+    constructor(string memory _name, string memory _symbol, uint256 _totalSupply, address _receiver) ERC20(_name, _symbol) {
         _mint(_receiver, _amount * 10 ** 18);
     }
 }
